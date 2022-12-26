fe4af321cff1efdd2363a5ba4ac80bbec9bb698e
kseniahwang16 <121436760+kseniahwang16@users.noreply.github.com>
2022-12-26 18:16:04 +0500

    добавлен класс Player

--- pingpong.py
+++ pingpong.py
@@ -6,6 +6,20 @@ win_heigh = 500
 mw = display.set_mode((win_width, win_heigh))
 mw.fill(back)
 
+class Player():
+    def update_l(self):
+        keys = key.get_pressed()
+        if keys[K_w] and self.rect.y > 5:
+            self.rect.y -= self.speed
+        if keys[K_s] and self.rect.y < 5:
+            self.rect.y += self.speed
+    def update_r(self):
+        keys = key.get_pressed()
+        if keys[K_UP] and self.rect.y > 5:
+            self.rect.y -= self.speed
+        if keys[K_DOWN] and self.rect.y < 5:
+            self.rect.y += self.speed
+
 clock = time.Clock()
 FPS = 40
 game = True
@@ -18,3 +32,4 @@ while game:
         if not finish:
             display.update()
     clock.tick(FPS)
+
